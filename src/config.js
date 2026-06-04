819a2c043096d6cf33a7a6076931567716fa4ad4
ChipButt <jameschipbutt@hotmail.com>
2026-06-04 21:39:44 +0100

    Split config module

---
+++ src/config.js
@@ -0,0 +1,23 @@
+export const CONFIG = {
+  secondsPerGameDay: 60,
+  baseAcclimatisationDays: 7,
+  missionSearchTarget: 10,
+  saveKey: 'jess_dog_army_save_v1',
+};
+
+export const RESOURCE_LABELS = {
+  protein: 'Protein',
+  foodBowls: 'Food Bowls',
+  waterBowls: 'Water Bowls',
+  blankets: 'Blankets',
+};
+
+export const MISSIONS = {
+  basic_rescue: {
+    id: 'basic_rescue',
+    name: 'Basic Rescue Mission',
+    foodCost: 3,
+    dogsRewarded: 1,
+    unlocked: true,
+  },
+};
